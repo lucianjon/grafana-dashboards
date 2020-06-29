7f1c605e0a2ebf7f8afdca50485b7793edebd781
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-29 21:02:00 +0200

    PMM-6138 Add tests for UpdatePanel

---
+++ pmm-app/src/pmm-update/UpdatePanel.test.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Button } from '@grafana/ui';
+
+import { InfoBox, ProgressModal } from 'pmm-update/components';
+import { usePerformUpdate, useVersionDetails } from 'pmm-update/hooks';
+import { UpdatePanel } from 'pmm-update/UpdatePanel';
+
+jest.mock('../react-plugins-deps/components/helpers/notification-manager');
+
+// NOTE (nicolalamacchia): these mocks are here because some test cases alter them
+jest.mock('./hooks/useVersionDetails', () => ({
+  useVersionDetails: jest.fn(),
+}));
+const mockedUseVersionDetails = useVersionDetails as jest.Mock;
+
+jest.mock('./hooks/usePerformUpdate', () => ({
+  usePerformUpdate: jest.fn(),
+}));
+const mockedUsePerformUpdate = usePerformUpdate as jest.Mock;
+
+const fakeLaunchUpdate = jest.fn();
+const fakeGetCurrentVersionDetails = jest.fn();
+
+const installedVersionDetails = {
+  installedFullVersion: 'a.b.c-rc.i+0123456789',
+  installedVersionDate: 'June 08',
+  installedVersion: 'a.b.c',
+};
+const nextVersionDetails = {
+  nextFullVersion: 'x.y.z-rc.j+1234567890',
+  nextVersionDate: 'June 09',
+  nextVersion: 'x.y.z',
+  newsLink: 'https://percona.com',
+};
+const lastCheckDate = 'June 10, 19:16';
+
+describe('UpdatePanel::', () => {
+  beforeEach(() => {
+    // default mocks
+    mockedUsePerformUpdate.mockImplementation(() => ['', '', false, false, fakeLaunchUpdate]);
+    mockedUseVersionDetails.mockImplementation(() => [
+      { installedVersionDetails, lastCheckDate, nextVersionDetails, isUpdateAvailable: true },
+      '',
+      true,
+      true,
+      fakeGetCurrentVersionDetails,
+    ]);
+  });
+
+  afterEach(() => {
+    mockedUsePerformUpdate.mockClear();
+    mockedUseVersionDetails.mockClear();
+  });
+
+  it('shows a box telling that no updates are available by default', () => {
+    const wrapper = shallow(<UpdatePanel />);
+
+    expect(wrapper.find(InfoBox).length).toEqual(1);
+
+    wrapper.unmount();
+  });
+
+  it('should return the correct values if the update initialization was successful', async () => {
+    const wrapper = shallow(<UpdatePanel />);
+
+    expect(mockedUseVersionDetails).toBeCalledTimes(1);
+
+    wrapper?.unmount();
+  });
+
+  test('the modal is closed by default', () => {
+    const wrapper = shallow(<UpdatePanel />);
+
+    const modal = wrapper.find(ProgressModal);
+
+    expect(modal.length).toEqual(1);
+    expect(modal.props()).toHaveProperty('isOpen', false);
+
+    wrapper.unmount();
+  });
+
+  it('should launch the update if the update button is clicked', async () => {
+    const wrapper = shallow(<UpdatePanel />);
+
+    wrapper?.find(Button).simulate('click');
+
+    expect(fakeLaunchUpdate).toBeCalledTimes(1);
+
+    wrapper?.unmount();
+  });
+});
